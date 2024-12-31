732d0ec2ad5a2d7560ef79c3bf096149458193bf
ritammandal-bit <rm386160@gmail.com>
2025-01-01 05:24:56 +0530

    first commit

---
+++ rock paper scissors.py
@@ -0,0 +1,34 @@
+import random
+
+computer = random.choice([-1,0,1])
+youtstr = input("Enter your choice (rock/paper/scissor): ")
+youDict = {"rock":-1,"paper":0,"scissor":1}
+reverseDict ={-1:"rock",0:"paper",1:"scissors"}
+you = youDict[youtstr]
+
+print(f"\nComputer chose {reverseDict[computer]}\nYour chosed {reverseDict[you]}")
+
+if(computer == you):
+    print("That is a draw Game")
+
+else:
+    if(computer == -1 and you == 1):
+        print("Rock smashes scissors! You win!")
+
+    elif(computer ==-1 and you == 0):
+        print("Rock smashes paper! Computer wins!")
+
+    elif(computer == 1 and you == -1):
+        print("Scissors cuts rock! Computer wins!")
+
+    elif(computer == 1 and you == 0):
+          print("Scissors cuts rock! Computer wins!")
+
+    elif(computer == 0 and you == -1):
+        print("Paper covers rock! You win!")
+
+    elif(computer == 0 and you == 1):
+        print("Paper covers rock! You win!")
+
+    else:
+        print("Some thing rong!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1")
